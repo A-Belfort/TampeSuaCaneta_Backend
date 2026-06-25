ebcc7704910786a40d4dd94ec764997af9f4a7a5
A-Belfort <arthur.ferreira0020016111@edu.pe.senac.br>
2026-06-25 21:20:02 +0000

    put host back

--- app.py
+++ app.py
@@ -28,4 +28,4 @@ def pegar_coords():
         return jsonify({"erro": "Erro ao pegar coordenadas."}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', debug=True, port=5000)
